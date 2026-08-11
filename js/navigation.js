c2ed956aa34891f0d93a8d1272e07a2c114e2be0
martin16pich <martin16pich@gmail.com>
2026-08-12 00:26:42 +0200

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -1,1 +1,12 @@
-a
+const menuButton = document.querySelector(".menu-toggle");
+const navigation = document.querySelector(".navigation");
+
+if (menuButton && navigation) {
+
+    menuButton.addEventListener("click", () => {
+
+        navigation.classList.toggle("show");
+
+    });
+
+}
