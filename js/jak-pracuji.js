bb03452155f6870a9aef5d2faa1dd160106b0ac6
martin16pich <martin16pich@gmail.com>
2026-08-12 16:17:53 +0200

    obrázky

--- js/jak-pracuji.js
+++ js/jak-pracuji.js
@@ -1,11 +1,8 @@
 let stranka = "povoleni";
 
 const image = document.getElementById("work-image");
-
 const title = document.getElementById("work-title");
-
 const text = document.getElementById("work-text");
-
 const extra = document.getElementById("work-extra");
 
 const menu = document.querySelectorAll(".work-menu button");
@@ -14,7 +11,12 @@ function zobraz(str){
 
     stranka = str;
 
-    image.src = work[str].image;
+    image.src = work[str].image
+        .replace("images/jak-pracuji/","images/jak-pracuji/hero/")
+        .replace(".jpg",".webp")
+        .replace(".png",".webp")
+        .replace(".jpeg",".webp")
+        .replace(".JPEG",".webp");
 
     title.textContent = work[str].title;
 
